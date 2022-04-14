0a24b30dcec6885cf58f95a6447ce733e180790e
caleb-melchizedek <codeprogrammer25112018@gmail.com>
2022-04-14 17:50:25 +0000

    home page created

--- _frontend/src/pages/index.js
+++ _frontend/src/pages/index.js
@@ -5,49 +5,54 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
+    <div className="bg-[url('/bg.jpg')] bg-cover bg-fixed">
       <Head>
-        <title>Make your Mark</title>
-        {/* <link rel="icon" href="" /> */}
+        <title>Sojourner</title>
       </Head>
-      <div className="relative top-20 flex flex-row items-center justify-center z-10 ">
-        <div
-          id="forms"
-          className="flex flex-col px-10 sm:px-20  items-center justify-center top-20 sm:top-20 fixed"
-        >
+      
+        <div className="flex flex-col  items-stretch  min-h-screen px-7 pt-7 z-10 backdrop-blur-sm">
+          <header id="landing-text" className="w-full">
+            <h1 className=" text-4xl sm:text-7xl font-bold  ">
+             Sojourner
+            </h1>
+            <h4 className="font-semibold  text-m">
+              Search for a suitable accommodation
+            </h4>
+          </header>
+            <div className="flex flex-row grow w-full items-center justify-center ">
+              <form className="search">
+                <div className="w-full flex justify-center mb-4 ">
+                  {/* whats your budget? lowest */}
+                  <div className="flex flex-col items-center m-4">
+                    <label className="text-white font-medium mb-2">I'll spend at least</label>
+                    <input className="" type="text" placeholder="GHC"></input>
+                  </div>
+                  {/* whats your budget? highest */}
+                  <div className="flex flex-col items-center m-4">
+                    <label className="text-white font-medium mb-2">I'll spend at most</label>
+                  <input type="text" placeholder="GHC"></input>
+                  </div>
+                  {/*  whats your budget? room capacity*/}
+                  <div className="flex flex-col items-center m-4">
+                    <label className="text-white font-medium mb-2">Room capacity</label>
+                  <input type="text" placeholder="People per room"></input>
+                  </div>
+                  {/* whats your budget? payment cycle */}
+                  <div className="flex flex-col items-center m-4">
+                    <label className="text-white font-medium mb-2">Payment cycle</label>
+                  <input type="text" placeholder="per month"></input>
+                  </div>
+                </div>
+                <button type="submit"> Search</button>
+              </form>
+            </div>
+            <footer className="flex items-center text-sm justify-center w-full h-10">
+              Powered by{" "}
+              <img src="/Mag_Mel-log.png" alt="some Logo" className="h-7 ml-4" />
+            </footer>
         </div>
+      
       </div>
-      <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <header id="landing-text" className="flex flex-col items-center my-24 ">
-          <h1 className=" text-4xl sm:text-7xl pb-4 font-semibold  ">
-            Make your mark
-          </h1>
-          <h4 className="font-semibold text-gray-400">
-            Tell your story, your way, Be a marker.{" "}
-          </h4>
-        </header>
-        <Link href="#featured-marks" passHref scroll>
-          <button className="btn-red w-20 sm:w-28 justify-self-end">View featured</button>
-        </Link>
-      </div>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-4 text-center">
-        <div className="w-full">
-          <h1 className="font-medium text-3xl sm:text-2xl pb-4">
-            Featured Marks
-          </h1>
-
-          <section
-            id="featured-marks"
-            className="flex flex-col md:flex-row md:space-x-12 justify-evenly pt-14 pb-10 w-full border-t border-b "
-          >
-          </section>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 ">
-        Powered by{" "}
-        <img src="/Mag_Mel-log.png" alt="some Logo" className="h-7 ml-4" />
-      </footer>
     </>
   );
 }
--- db/RoomSchema.js
+++ db/RoomSchema.js
@@ -9,9 +9,9 @@ const mongoose = require('mongoose'),
         required: true,
       },
       pricing: [{
-        currency:{type:String,enum:["GHS","USD"],default:"GHS"},
+        currency:{type:String,enum:["GHS","USD"],default:"GHS", required:true},
         amount:{type:Number,required:true},
-        paymentCycle: {type:String, enum:["month","semester","year"]},
+        paymentCycle: {type:String, enum:["month","semester","year"], required:true},
         residentType:{type:String,enum:["Student","NSS","Any"], default:"Any"},
       }],
       extraFeatures:[{extraFeature:String}]
