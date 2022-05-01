8d13c71f0d106893e43f40048ccabe5e5edd1bdb
caleb-melchizedek <codeprogrammer25112018@gmail.com>
2022-05-01 18:59:08 +0000

    fixed more search bugs

--- db/search.js
+++ db/search.js
@@ -5,35 +5,69 @@ async function handler( req, res){
   //res.json(`you searched for rooms with these params ${lowestPrice}, ${highestPrice},${roomCapacity},${paymentCycle}`)
 try{
   var results
-    // if(lowestPrice==="" && highestPrice==="" && roomCapacity==="" && paymentCycle===""){
-      results = await room.find().or(
-        [
-          {"pricing.amount":{$gte:lowestPrice, $lte:highestPrice},"roomCapacity":roomCapacity,"pricing.paymentCycle":paymentCycle},
-          
-          {"pricing.amount":{$lte:highestPrice},"roomCapacity":roomCapacity,"pricing.paymentCycle":paymentCycle},
-          {"pricing.amount":{$gte:lowestPrice},"roomCapacity":roomCapacity,"pricing.paymentCycle":paymentCycle},
-          {"pricing.amount":{$gte:lowestPrice, $lte:highestPrice},"pricing.paymentCycle":paymentCycle},
-          {"pricing.amount":{$gte:lowestPrice, $lte:highestPrice},"roomCapacity":roomCapacity},
+   
+  // only three inputs
+  if(lowestPrice!=="" && highestPrice!=="" && roomCapacity!=="" && paymentCycle===""){
+    results = await room.find({"pricing.amount":{$gte:lowestPrice, $lte:highestPrice},"roomCapacity":roomCapacity}).select({"__v":0}).populate("facility",{"_id":0, "__v":0})
+  }
 
-          {"pricing.amount":{$gte:lowestPrice, $lte:highestPrice}},
-          {"pricing.amount":{$gte:lowestPrice},"roomCapacity":roomCapacity,},
-          {"pricing.amount":{$gte:lowestPrice},"pricing.paymentCycle":paymentCycle},
-          {"pricing.amount":{ $lte:highestPrice},"roomCapacity":roomCapacity},
-          {"pricing.amount":{$lte:highestPrice},"pricing.paymentCycle":paymentCycle},
-          {"roomCapacity":roomCapacity,"pricing.paymentCycle":paymentCycle},
+  if(lowestPrice!=="" && highestPrice!=="" && roomCapacity==="" && paymentCycle!==""){
+    results = await room.find({"pricing.amount":{$gte:lowestPrice, $lte:highestPrice},"pricing.paymentCycle":paymentCycle}).select({"__v":0}).populate("facility",{"_id":0, "__v":0})
+  }
 
-          {"pricing.amount":{$gte:lowestPrice}},
-          {"pricing.amount":{ $lte:highestPrice}},
-          {"roomCapacity":roomCapacity,},
-          {"pricing.paymentCycle":paymentCycle},
-          // {"roomCapacity":roomCapacity},
-          // {"pricing.paymentCycle":paymentCycle}
-        ])
-      .select({"__v":0})
-      .populate("facility",{"_id":0, "__v":0});
-    // }
+  if(lowestPrice!=="" && highestPrice==="" && roomCapacity!=="" && paymentCycle!==""){
+    results = await room.find({"pricing.amount":{$gte:lowestPrice},"roomCapacity":roomCapacity,"pricing.paymentCycle":paymentCycle}).select({"__v":0}).populate("facility",{"_id":0, "__v":0})
+  }
+
+  if(lowestPrice==="" && highestPrice!=="" && roomCapacity!=="" && paymentCycle!==""){
+    results = await room.find({"pricing.amount":{$lte:highestPrice},"roomCapacity":roomCapacity,"pricing.paymentCycle":paymentCycle}).select({"__v":0}).populate("facility",{"_id":0, "__v":0})
+  }
   
 
+  // only two inputs
+  if(lowestPrice!=="" && highestPrice!=="" && roomCapacity==="" && paymentCycle===""){
+    results = await room.find({"pricing.amount":{$gte:lowestPrice, $lte:highestPrice}}).select({"__v":0}).populate("facility",{"_id":0, "__v":0})
+  }
+  if(lowestPrice!=="" && highestPrice==="" && roomCapacity!=="" && paymentCycle===""){
+    results = await room.find({"pricing.amount":{$gte:lowestPrice},"roomCapacity":roomCapacity}).select({"__v":0}).populate("facility",{"_id":0, "__v":0})
+  }
+
+  if(lowestPrice!=="" && highestPrice==="" && roomCapacity==="" && paymentCycle!==""){
+    results = await room.find({"pricing.amount":{$gte:lowestPrice},"pricing.paymentCycle":paymentCycle}).select({"__v":0}).populate("facility",{"_id":0, "__v":0})
+  }
+
+  if(lowestPrice==="" && highestPrice!=="" && roomCapacity!=="" && paymentCycle===""){
+    results = await room.find({"pricing.amount":{ $lte:highestPrice},"roomCapacity":roomCapacity}).select({"__v":0}).populate("facility",{"_id":0, "__v":0})
+  }
+
+  if(lowestPrice==="" && highestPrice!=="" && roomCapacity==="" && paymentCycle!==""){
+    results = await room.find( {"pricing.amount":{$lte:highestPrice},"pricing.paymentCycle":paymentCycle}).select({"__v":0}).populate("facility",{"_id":0, "__v":0})
+  }
+
+  if(lowestPrice==="" && highestPrice==="" && roomCapacity!=="" && paymentCycle!==""){
+    results = await room.find({"roomCapacity":roomCapacity,"pricing.paymentCycle":paymentCycle}).select({"__v":0}).populate("facility",{"_id":0, "__v":0})
+  }
+
+  // only one input
+
+  if(lowestPrice!=="" && highestPrice==="" && roomCapacity==="" && paymentCycle===""){
+    results = await room.find({"pricing.amount":{ $gte:lowestPrice}}).select({"__v":0}).populate("facility",{"_id":0, "__v":0})
+  }
+  if(lowestPrice==="" && highestPrice!=="" && roomCapacity==="" && paymentCycle===""){
+    results = await room.find({"pricing.amount":{ $lte:highestPrice}}).select({"__v":0}).populate("facility",{"_id":0, "__v":0})
+  }
+  if(lowestPrice==="" && highestPrice==="" && roomCapacity!=="" && paymentCycle===""){
+    results = await room.find({"roomCapacity":roomCapacity}).select({"__v":0}).select({"__v":0}).populate("facility",{"_id":0, "__v":0})
+  }
+  if(lowestPrice==="" && highestPrice==="" && roomCapacity==="" && paymentCycle!==""){
+    results = await room.find({"pricing.paymentCycle":paymentCycle}).select({"__v":0}).select({"__v":0}).populate("facility",{"_id":0, "__v":0})
+  }
+    
+  // no inputs
+  if(lowestPrice==="" && highestPrice==="" && roomCapacity==="" && paymentCycle===""){
+    results = await room.find({}).select({"__v":0}).populate("facility",{"_id":0, "__v":0});
+  }
+  
     if(results.length>0){
       console.log(results)
       res.status(200).json(results)
