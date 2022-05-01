a88bdf44220754ad121f5a8d0efc642e7f00bd3c
caleb-melchizedek <codeprogrammer25112018@gmail.com>
2022-05-01 17:57:14 +0000

    adjusted search algorithm

--- db/search.js
+++ db/search.js
@@ -2,23 +2,38 @@ const room = require('./roomSchema');
 async function handler( req, res){
   let {lowestPrice,highestPrice,roomCapacity,paymentCycle} = req.body
   console.log({lowestPrice,highestPrice,roomCapacity,paymentCycle})
-  if(lowestPrice==='')lowestPrice=null;
-  if(highestPrice==='')highestPrice=null;
-  if(roomCapacity==='')roomCapacity=null;
-  if(paymentCycle==='')paymentCycle=null;
   //res.json(`you searched for rooms with these params ${lowestPrice}, ${highestPrice},${roomCapacity},${paymentCycle}`)
 try{
-    results = await room.find(
-        {
-          $or: [
-              {"pricing.amount":{$gte:lowestPrice, $lte:highestPrice},"roomCapacity":roomCapacity,"pricing.paymentCycle":paymentCycle},
-              // {"roomCapacity":roomCapacity},
-              // {"pricing.paymentCycle":paymentCycle}
-          ]
-        },
-        {"__v":0}
-      )
+  var results
+    // if(lowestPrice==="" && highestPrice==="" && roomCapacity==="" && paymentCycle===""){
+      results = await room.find().or(
+        [
+          {"pricing.amount":{$gte:lowestPrice, $lte:highestPrice},"roomCapacity":roomCapacity,"pricing.paymentCycle":paymentCycle},
+          
+          {"pricing.amount":{$lte:highestPrice},"roomCapacity":roomCapacity,"pricing.paymentCycle":paymentCycle},
+          {"pricing.amount":{$gte:lowestPrice},"roomCapacity":roomCapacity,"pricing.paymentCycle":paymentCycle},
+          {"pricing.amount":{$gte:lowestPrice, $lte:highestPrice},"pricing.paymentCycle":paymentCycle},
+          {"pricing.amount":{$gte:lowestPrice, $lte:highestPrice},"roomCapacity":roomCapacity},
+
+          {"pricing.amount":{$gte:lowestPrice, $lte:highestPrice}},
+          {"pricing.amount":{$gte:lowestPrice},"roomCapacity":roomCapacity,},
+          {"pricing.amount":{$gte:lowestPrice},"pricing.paymentCycle":paymentCycle},
+          {"pricing.amount":{ $lte:highestPrice},"roomCapacity":roomCapacity},
+          {"pricing.amount":{$lte:highestPrice},"pricing.paymentCycle":paymentCycle},
+          {"roomCapacity":roomCapacity,"pricing.paymentCycle":paymentCycle},
+
+          {"pricing.amount":{$gte:lowestPrice}},
+          {"pricing.amount":{ $lte:highestPrice}},
+          {"roomCapacity":roomCapacity,},
+          {"pricing.paymentCycle":paymentCycle},
+          // {"roomCapacity":roomCapacity},
+          // {"pricing.paymentCycle":paymentCycle}
+        ])
+      .select({"__v":0})
       .populate("facility",{"_id":0, "__v":0});
+    // }
+  
+
     if(results.length>0){
       console.log(results)
       res.status(200).json(results)
