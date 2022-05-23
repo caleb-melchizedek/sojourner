a27712dbe7055c56e5d164e8f6478d660ce9127d
caleb-melchizedek <codeprogrammer25112018@gmail.com>
2022-05-23 12:37:32 +0000

    image selector to pick only images

--- _frontend/src/components/addFacilityForm.js
+++ _frontend/src/components/addFacilityForm.js
@@ -6,10 +6,10 @@ export default function AddFacilityForm (props){
             <label className="text-xs text-gray-500 font-medium mb-3   ">Edit Facility Info</label>
             <input className="input" type="text" required name="facilityname" placeholder="Name" value={facilityInfo.facilityname} onChange={(e)=>{handleFacilityDetailsChange(e)}}></input>
             <input className="input" type="email"  name="email" placeholder="Email" value={facilityInfo.email} onChange={(e)=>{handleFacilityDetailsChange(e)}}></input>
-            <input className="input" type="Number" required name="tel" placeholder="Telephone" value={facilityInfo.tel} onChange={(e)=>{handleFacilityDetailsChange(e)}}></input>
+            <input className="input" type="Number" name="tel" placeholder="Telephone" value={facilityInfo.tel} onChange={(e)=>{handleFacilityDetailsChange(e)}}></input>
             <textarea required name="location" placeholder="Location" value={facilityInfo.location} onChange={(e)=>{handleFacilityDetailsChange(e)}}></textarea>
             <label className="text-xs text-gray-500 font-medium  ">Add photos of the facility</label>
-            <input multiple className="inputfile" type="file" required name="images" placeholder="Images" accept="image/png, image/jpg," onChange={(e)=>{handleFacilityDetailsChange(e)}}></input>
+            <input multiple className="inputfile" type="file" required name="images" placeholder="Images" accept="image/*" onChange={(e)=>{handleFacilityDetailsChange(e)}}></input>
             <div className="flex flex-nowrap px-4 justify-center"><p className=" text-red-600 text-xs ">{error}</p></div>
             
             {
