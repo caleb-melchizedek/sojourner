a7ca50575a3c4ce3876dbc4a50c43ae27d95e5c8
caleb-melchizedek <codeprogrammer25112018@gmail.com>
2022-06-02 05:29:56 +0000

    added landing page and adjusted some styling

--- _frontend/src/pages/index.js
+++ _frontend/src/pages/index.js
@@ -120,7 +120,7 @@ const toggleHelp= ()=>{
             </span>
           </h1>
           
-          <Link href="/home"><button className="self-center h-12 w-48 md:w-1/3 md:h-10 md:mt-7 p-2 text-white  bg-blue-500 font-semibold text-xs md:text-base hover:bg-white hover:text-black  transition-all">Search</button></Link>
+          <Link href="/home"><button className="self-center h-12 w-48 mt-28 md:w-1/3 md:h-10 md:mt-30 p-2 text-white  bg-blue-500 font-semibold text-xs md:text-base hover:bg-white hover:text-black  transition-all">Search</button></Link>
           <div className=" fixed bottom-2 right-2 flex flex-col w-full items-end justify-end overflow-hidden ">
             <div className="help-text" >
               <p className="text-sm px-4 py-2 items-center justify-center ">
